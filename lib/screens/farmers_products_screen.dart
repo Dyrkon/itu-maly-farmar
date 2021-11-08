8fab3de5f8bfee5751f4780314c59648d710ffa9
Adam Jetmar <rohliksjogurtem@seznam.cz>
2021-11-08 09:41:46 +0100

    add farmer widget

--- lib/screens/farmers_products_screen.dart
+++ lib/screens/farmers_products_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maly_farmar/icons/custom_icons.dart';
+import '../widgets/farmer_widget.dart';
 
 class FarmersProductsScreen extends StatelessWidget {
   const FarmersProductsScreen({Key? key}) : super(key: key);
@@ -8,7 +9,7 @@ class FarmersProductsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          body: Center(child: Text("Farmers"),),
+          body: Center(child: farmerWidget()),
         ),
 
     );
--- lib/widgets/farmer_widget.dart
+++ lib/widgets/farmer_widget.dart
@@ -1,1 +1,29 @@
 import 'package:flutter/material.dart';
+
+class farmerWidget extends StatelessWidget {
+  farmerWidget({Key? key}) : super(key: key);
+
+  //farmerWidget({Key key, this.name, this.amount, this.pricPerUnit, this.unit, this.profilePicture})
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Row(
+          children: <Widget>[
+           Container(
+          decoration: new BoxDecoration(
+            image: new DecorationImage(image: new AssetImage("../../assets/images/profile_picture_test.png"), fit: BoxFit.cover,),
+          ),
+            Column(
+              children: [
+               Container(child: const Text("Vojtěch Novák")),
+               Container(child: const Text("60ks"))
+              ]
+              ,
+              ),
+              Column(children: [const Text("4Kč/ks")])
+          ],
+        ),
+      );
+    }
+}
