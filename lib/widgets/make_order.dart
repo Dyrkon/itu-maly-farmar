2f6536b25c3baa71cb0ee5763ff2908a29aaffe3
Dyrkon <dyrkon603@gmail.com>
2021-11-16 16:12:29 +0100

    Make order and order widget ready for testing

--- lib/widgets/make_order.dart
+++ lib/widgets/make_order.dart
@@ -1,1 +1,47 @@
 import 'package:flutter/material.dart';
+import '/icons/custom_icons.dart';
+import '/models/order.dart';
+import '/colors/colors.dart';
+
+class MakeOrderWidget extends StatelessWidget {
+  var options = List.generate(50, (index) => "$index");
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          const Text("Vaše objednávka"),
+          Row(
+            children: [
+              const Text("Vyberte množství: "),
+              DropdownButton(
+                value: options[0],
+                items: options.map((var option) {
+                  return DropdownMenuItem(
+                    child: Text(option),
+                    value: int.parse(option),
+                  );
+                }).toList(),
+                onChanged: (Object? value) {},
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text("Vyberte datum: "),
+              // TODO add calendar picker
+            ],
+          ),
+          const Text("Vaše rezervace bude předložena prodejci"),
+          const Text("Stav své rezervace můžete sledovat v seznamu objednávek"),
+          ElevatedButton(
+              onPressed: () => {},
+              child: const Text("Potvrdit rezervaci"),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/order_widget.dart
+++ lib/widgets/order_widget.dart
@@ -3,20 +3,30 @@ import 'package:maly_farmar/icons/custom_icons.dart';
 import 'package:maly_farmar/models/order.dart';
 import '../colors/colors.dart';
 
-class OrderWidget extends StatelessWidget {
+class OrderWidget extends StatefulWidget {
   final String product;
   final String name;
-  int confirmed;
+  bool isConfirmed;
   int quantity;
   DateTime date;
 
-  OrderWidget(this.product, this.name, this.quantity, this.date, this.confirmed,
+  OrderWidget(
+      this.product, this.name, this.quantity, this.date, this.isConfirmed,
       {Key? key})
       : super(key: key);
 
   @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        color:
+            (widget.isConfirmed ? Palette.farmersGreen.shade300 : Colors.white),
+      ),
       child: Row(
         children: [
           Container(
@@ -24,20 +34,26 @@ class OrderWidget extends StatelessWidget {
               ),
           Column(
             children: [
-              Text(product),
+              Text(widget.product),
               const Text("Množství:"),
-              Text(quantity.toString()),
+              Text(widget.quantity.toString()),
             ],
           ),
           Column(
             children: [
-              Text(name),
+              Text(widget.name),
               const Text("Datum"),
-              Text(date.day.toString() + " " + date.month.toString()),
+              Text(widget.date.day.toString() +
+                  " " +
+                  widget.date.month.toString()),
             ],
           ),
-           const Icon(CustomIcons.cross),
-          const Icon(CustomIcons.check),
+          widget.isConfirmed
+              ? const SizedBox.shrink()
+              : const Icon(CustomIcons.cross),
+          widget.isConfirmed
+              ? const SizedBox.shrink()
+              : const Icon(CustomIcons.check),
         ],
       ),
     );
