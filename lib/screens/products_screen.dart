343b7a35d0b243787303d606029ad01d4473753c
Ondra <xkrizo05@stud.fit.vutbr.cz>
2021-12-02 22:07:40 +0100

    add product screen values checks added

--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -8,13 +8,12 @@ class ProductsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     return const SafeArea(
       child: Scaffold(
-        body: Center(child: Text("Farmers product screen"),),
+        body: Center(
+          child: Text("Farmers product screen"),
+        ),
       ),
-
     );
   }
 }
