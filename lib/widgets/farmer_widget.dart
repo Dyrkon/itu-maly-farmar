584a79b0edd305d7aca0587c2ba390027527b02b
Jetyas <rohliksjogurtem@seznam.cz>
2021-12-02 12:50:20 +0100

    Farmer widget updated

--- lib/widgets/farmer_widget.dart
+++ lib/widgets/farmer_widget.dart
@@ -1,29 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:maly_farmar/models/product.dart';
+import 'package:maly_farmar/models/user.dart';
 
-class farmerWidget extends StatelessWidget {
-  farmerWidget({Key? key}) : super(key: key);
+class FarmerWidget extends StatelessWidget {
+  final UserProfile farmer;
+  final Product product; // TODO chceme produkt daneho farmare
 
-  //farmerWidget({Key key, this.name, this.amount, this.pricPerUnit, this.unit, this.profilePicture})
+  const FarmerWidget(this.farmer, this.product, {Key? key}) : super(key : key);
 
+ @override
+  State<StatefulWidget> createState() => _FarmerWidgetState();
+}
+
+class _FarmerWidgetState extends State<FarmerWidget> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Row(
-          children: <Widget>[
-           Container(
-          decoration: new BoxDecoration(
-            image: new DecorationImage(image: new AssetImage("../../assets/images/profile_picture_test.png"), fit: BoxFit.cover,),
-          ),
+    var farmer = widget.farmer;
+    var product = widget.product;
+
+    return Container(
+        decoration: BoxDecoration(
+            color: Colors.grey[200], borderRadius: BorderRadius.circular(10)),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 1 / 9,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.height * 1 / 9,
+              height: MediaQuery.of(context).size.height * 1 / 9,
+              decoration: const BoxDecoration(
+                  borderRadius:
+                      BorderRadius.horizontal(left: Radius.circular(10)),
+                  image: DecorationImage(
+                      fit: BoxFit.fitHeight,
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1589923188900-85dae523342b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"))),
+            ),
             Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-               Container(child: const Text("Vojtěch Novák")),
-               Container(child: const Text("60ks"))
-              ]
-              ,
-              ),
-              Column(children: [const Text("4Kč/ks")])
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+                  child: Text(
+                    farmer.fullName,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            product.accessibleAmount.toString(), // FIXME
+                            style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 30,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            product.unit, //FIXME cena za jednotku
+                            style: const TextStyle(
+                              fontSize: 17,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
           ],
-        ),
-      );
-    }
+        ));
+  }
 }
