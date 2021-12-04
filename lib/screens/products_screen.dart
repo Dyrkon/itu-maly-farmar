a4ec59a1b327615104c05c39c16babdb8c969454
Jetyas <rohliksjogurtem@seznam.cz>
2021-12-04 19:54:32 +0100

    Products screen search buttons added

--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -16,6 +16,8 @@ class ProductsScreen extends StatefulWidget {
 }
 
 class _ProductsScreenState extends State<ProductsScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
   final offerData = Provider.of<Offers>(context);
@@ -29,7 +31,159 @@ class _ProductsScreenState extends State<ProductsScreen> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          actions: <Widget> 
+          backgroundColor: Colors.transparent,
+          bottomOpacity: 0.0,
+          elevation: 0.0,
+          actions: <Widget> [
+           Align(
+             alignment: Alignment.topLeft,
+            child: SizedBox(
+              height: 30,
+              width: 100,
+            child: ElevatedButton(
+               style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                side: BorderSide(
+                  width: 1.5,
+                  color: Colors.black,
+                ),
+              ),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: Stack(
+                        children: [
+                          Positioned(
+                            right: -40.0,
+                            top: -40.0,
+                            child: InkResponse(
+                              onTap: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: CircleAvatar(
+                                child: Icon(Icons.close),
+                                backgroundColor: Colors.red,
+                              ),
+                              ),
+                          ),
+                          Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text("Zadej hledaný produkt:"),
+                                Padding(
+                                  padding: EdgeInsets.all(3.0),
+                                  child: TextFormField(),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(3.0),
+                                  child: ElevatedButton(
+                                    child: Text("Hledat produkt"),
+                                    onPressed: () {
+                                      if(_formKey.currentState!.validate()) {
+                                        _formKey.currentState?.save();
+                                      }
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+                  },
+                  child: const Text(
+                    "Vejce",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                    ),
+              
+            ),
+            ),
+            ),
+
+            // 2. BUUUUTOOOOOOOOOOOOON
+            Align(
+             alignment: Alignment.topRight,
+            child: SizedBox(
+              height: 30,
+              width: 100,
+            child: ElevatedButton(
+               style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                side: BorderSide(
+                  width: 1.5,
+                  color: Colors.black,
+                ),
+              ),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: Stack(
+                        children: [
+                          Positioned(
+                            right: -40.0,
+                            top: -40.0,
+                            child: InkResponse(
+                              onTap: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: CircleAvatar(
+                                child: Icon(Icons.close),
+                                backgroundColor: Colors.red,
+                              ),
+                              ),
+                          ),
+                          Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text("Zadej místo vyzvednutí:"),
+                                Padding(
+                                  padding: EdgeInsets.all(3.0),
+                                  child: TextFormField(),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(3.0),
+                                  child: ElevatedButton(
+                                    child: Text("Hledat lokalitu"),
+                                    onPressed: () {
+                                      if(_formKey.currentState!.validate()) {
+                                        _formKey.currentState?.save();
+                                      }
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+                  },
+                  child: const Text(
+                    "Přerov",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                    ),
+              
+            ),
+            ),
+            ),
+
+          ]
+        /*  actions: <Widget> 
             [
               Row( // row for the two dropdown forms
                 children: <Widget>[
@@ -50,6 +204,7 @@ class _ProductsScreenState extends State<ProductsScreen> {
               ],  
           ),
         ],     
+        */
         ),
         body: RefreshIndicator(
           onRefresh: offerData.fetchOffers,
